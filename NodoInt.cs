c25788ea5fd53a32f229ca6eda3909b1c9ccc373
Josué Antonio Chaverri Aguilar <120685586+josuech21@users.noreply.github.com>
2023-06-17 11:35:20 -0600

    Update NodoInt.cs

--- NodoInt.cs
+++ NodoInt.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Capa_Modelo.Nodo
+{
+    public class NodoInt : Nodo<int,NodoInt>
+    {
+        public override int Valor { get; set; }
+        public override NodoInt Siguiente { get; set; }
+
+        public NodoInt(int _valor)
+        {
+            Valor = _valor;
+            Siguiente = null;
+        }
+    }
+}
